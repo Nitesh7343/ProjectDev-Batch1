2e7f3e876d98c71526d4757ab819cb837fbc1c7a
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-09 23:28:10 +0530

    boilerplate updated

--- Frontend/ParkSync/src/main.jsx
+++ Frontend/ParkSync/src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import NavBar from '../public/components/NavBar.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <NavBar/>
+);
