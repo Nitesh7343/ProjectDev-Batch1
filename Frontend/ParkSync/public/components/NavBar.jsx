fda5211bf8790948711d1d3155f99a8d0291a49b
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-09 23:27:49 +0530

    Componets ready to code

---
+++ Frontend/ParkSync/public/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <navbar>
+        <ul>
+            <li>Home</li>
+            <li>Parking Slot</li>
+            <li>Contact</li>
+        </ul>
+      </navbar>
+    </div>
+  )
+}
+
+export default NavBar
