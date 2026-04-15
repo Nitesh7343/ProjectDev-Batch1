993a95a7fcee981d6737b83b2f496be91d48bc81
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-15 23:37:37 +0530

    User.js model updated

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
+  vehicle_no: String,
+  location: String
 });
 
 export default mongoose.model("User", userSchema);
