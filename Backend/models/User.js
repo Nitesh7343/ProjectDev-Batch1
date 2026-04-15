11459d8e5d3d5e8cceba4c711d867b1ee9315f2b
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-15 23:38:54 +0530

    Reset User.js model to initial state

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,8 +4,6 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  vehicle_no: String,
-  location: String
 });
 
 export default mongoose.model("User", userSchema);
